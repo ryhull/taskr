74b7986ee4232bdcbe31e3d212bb34c4c04ed0fa
Ry <68780806+Ry-Projects@users.noreply.github.com>
2021-05-19 19:44:13 -0400

    Redesigned tasks, moved the color panel, task functionality

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
-const input = document.querySelector("#taskInput");
-const addBtn = document.querySelector("#confirmTask");
+const input = document.querySelector("#task-input");
+const addBtn = document.querySelector("#confirm-task");
 const list = document.querySelector("ul");
 const delAll = document.querySelector("#reset");
+const swapBtn = document.querySelector("#swap-color");
+const slideBtn = document.querySelector("#slide-btn");
+const bottomSlider = document.querySelector(".bottom-slider");
 var color = document.querySelector("#color").value;
-const swapBtn = document.querySelector("#swapColor");
-const slideBtn = document.querySelector("#slideBtn");
-const bottomSlider = document.querySelector(".bottomSlider");
 
 addBtn.addEventListener("click", addTask);
 delAll.addEventListener("click", deleteAll);
 swapBtn.addEventListener("click", colorSwap);
 slideBtn.addEventListener("click", slidePanel);
 
-input.addEventListener("keyup", function (event) {
+function addTask() {
+  if (input.value == "") {
+    alert("Add a task :)");
+  } else {
+    li = document.createElement("li");
+    li.innerHTML = `<p class="todo" onclick="strikeText(event)">${input.value}<span class="remove-btn" onclick="removeTask(event)">X</span></p>`;
+    list.append(li);
+    input.value = "";
+  }
+}
+
+//Allowing enter key to submit tasks
+input.addEventListener("keyup", (event) => {
   if (event.keyCode === 13) {
     event.preventDefault();
     addBtn.click();
   }
 });
 
-function addTask() {
-  if (input.value == "") {
-    alert("Add a task! :)");
-  } else {
-    li = document.createElement("li");
-    li.innerHTML = "<a>X</a><p>" + input.value + "</p>";
-    list.append(li);
-    input.value = "";
+//Show task as 'completed' when clicked
+function strikeText(event) {
+  switch (event.currentTarget.style.textDecoration) {
+    case "line-through":
+      event.currentTarget.style.textDecoration = "none";
+      event.currentTarget.style.opacity = "1";
+      break;
+    default:
+      event.currentTarget.style.textDecoration = "line-through";
+      event.currentTarget.style.opacity = "0.8";
+      break;
   }
 }
 
-function deleteTask() {
-  pass; //todo
+function removeTask(event) {
+  event.target.parentNode.parentNode.remove();
 }
 
 function deleteAll() {
@@ -41,11 +56,16 @@ function deleteAll() {
   input.value = "";
 }
 
+//Open or close the color panel
 function slidePanel() {
-  if (bottomSlider.style.height == "") {
-    bottomSlider.style.height = "150px";
-  } else if (bottomSlider.style.height == "150px") {
-    bottomSlider.style.height = "";
+  if (bottomSlider.style.width == "") {
+    bottomSlider.style.width = "250px";
+    bottomSlider.style.opacity = "1"
+    slideBtn.title = "Close Color Panel";
+  } else if (bottomSlider.style.width == "250px") {
+    bottomSlider.style.width = "";
+    bottomSlider.style.opacity = "0"
+    slideBtn.title = "Open Color Panel";
   }
 }
 
